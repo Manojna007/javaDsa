9150fadfecd83bfc3cbe544746673eefaadc6ac5
manojnap <1rn19cs076.manojnap@gmail.com>
2022-08-07 17:56:54 +0530

    new

--- src/LinkedList/linkedList_basics.java
+++ src/LinkedList/linkedList_basics.java
@@ -60,14 +60,14 @@ public class linkedList_basics {
         Node<Integer> n3 = new Node<>(30);
         Node<Integer> n4 = new Node<>(40);
         Node<Integer> n5 = new Node<>(50);
-//		Node<Integer> n6 = new Node<>(60);
+        Node<Integer> n6 = new Node<>(60);
 
         Node<Integer> head = n1;
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-//		n5.next = n6;
+		n5.next = n6;
 
         printLL(head);
 		head = insertLL(100, 6, head);
