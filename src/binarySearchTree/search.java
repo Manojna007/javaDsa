8c9b30417b7042a90643d525e35748850f4f4d35
manojnap <1rn19cs076.manojnap@gmail.com>
2022-08-27 00:03:37 +0530

    new

--- src/binarySearchTree/search.java
+++ src/binarySearchTree/search.java
@@ -21,6 +21,27 @@ public class search {
         return  false;
     }
     public static void main(String[] args){
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.right = new Node(80);
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+        root.right.left = new Node(70);
+        root.right.right = new Node(100);
+        root.right.left.left = new Node(60);
 
+        int key = 60;
+        System.out.println(isPresent(root, key));
+        System.out.println("iterative search :"+isPresentIterative(root, key));
+
+    }
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
     }
 }
